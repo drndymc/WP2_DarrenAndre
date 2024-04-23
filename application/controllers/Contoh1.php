6eba03f740c2bda1a7a6249b525c1ac88076a546
drndymc <darren.pratomo096@gmail.com>
2024-04-23 22:04:20 +0700

    upload pertemuan 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"nama saya Darren Pratomo Andre
+        READY TOP UP ALL GAME, APK PREMIUM & JOKI GAME ML, AOV & HOK
+        CP? @drndymc";
+    }
+}
+?>
