0f0b35281e48d23f54dcb1e0c8f0915543bd97a7
drndymc <darren.pratomo096@gmail.com>
2024-04-23 22:31:53 +0700

    upload tugas 4

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1> <?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+    <li>Nama Depan : Darren</li>
+    <li>Nama Belakang : Andre</li>
+ </ul>
+ <br>
+
+ <h4>Alamat</h4>
+ <ul type="none">
+    <li> BTR</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+    <li>Jakarta</li>
+ </ul>
+
+ <h4>Hoby</h4>
+ <ul type="square">
+    <li>Gamess</li>
+ </ul>
+</section>
